67b1e7455bbc6d65373fb3f8748ea5ab81111f09
ngatmaitan <ngatmaitan@smu.edu>
2016-09-25 18:30:10 -0500

    got the timer working.. code tested from jsfiddle

--- master.js
+++ master.js
@@ -2,6 +2,48 @@ var result;
 var userchoice;
 var userwins = 0;
 var botwins = 0;
+var ties = 0;
+var total = 0;
+var minutes = 0;
+var seconds = 0;
+var yourUl = document.getElementById("yourUlId");
+    
+    document.getElementById("submit").addEventListener("click", function(){
+    minutes = document.getElementById("minutes").value;
+    seconds = document.getElementById("seconds").value;
+    total = Number(minutes*60) + Number(seconds);
+    
+    var display = document.querySelector('#time'),
+        timer = new CountDownTimer(Number(total)),
+        timeObj = CountDownTimer.parse(Number(total));
+        
+        format(timeObj.minutes, timeObj.seconds);
+    
+    timer.onTick(format);
+    
+    timer.start();
+    
+    function format(minutes, seconds) {
+        if (minutes === 0 && seconds === 10){
+          document.getElementById("warning").style.display = '';
+        }
+        else if (minutes === 0 && seconds === 7){
+          document.getElementById("warning").style.display = 'none';
+        }
+        else if (minutes === 0 && seconds === 0){
+          if (userwins > botwins){
+            document.getElementById("winner").innerHTML = "user won.";
+          }
+          else if (userwins < botwins){
+            document.getElementById("winner").innerHTML = "bot won.";
+          }
+        }
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+        display.textContent = minutes + ':' + seconds;
+    }
+});
 
 document.getElementById("rock").addEventListener("click", function(){
     play(this);
@@ -16,39 +58,33 @@ document.getElementById("scissors").addEventListener("click", function(){
 });
 
 function displayBotChoice(botchoice){
-	var elt = document.getElementById("botchoice");
   if (botchoice == 0){
-  	elt.innerHTML = "Rock";
+    document.getElementById("botchoice").innerHTML = "Rock";
   }
   else if (botchoice == 1){
-  	elt.innerHTML = "Paper";
+    document.getElementById("botchoice").innerHTML = "Paper";
   }
   else{
-  	elt.innerHTML = "Scissors";
+    document.getElementById("botchoice").innerHTML = "Scissors";
   }
-}
-
-function displayResult(res){
-	document.getElementById("result").innerHTML = res;
-	document.getElementById("userwins").innerHTML = userwins;
-  document.getElementById("botwins").innerHTML = botwins;
 }
 
 function play(userinput){
-		var botchoice = Math.floor(Math.random() * 3);
+    var botchoice = Math.floor(Math.random() * 3);
     displayBotChoice(botchoice);
     document.getElementById("userchoice").innerHTML = userinput.innerHTML;
-		
+    
     result = (3 + userinput.value - botchoice) % 3;
     if (result == 1){
-    		userwins++;
-        return displayResult("user wins");
+        userwins++;
+        document.getElementById("userwins").innerHTML = userwins;
     }
     else if (result == 2){
-    		botwins++;
-        return displayResult("bot wins");
+        botwins++;
+        document.getElementById("botwins").innerHTML = botwins;
     }
     else{
-        return displayResult("it's a tie");
+        ties++;
+        document.getElementById("ties").innerHTML = ties;
     }
 }
