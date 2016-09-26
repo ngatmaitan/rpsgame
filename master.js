dad6905a7175417dbb16957a2890d6e57a2ddff0
ngatmaitan <ngatmaitan@smu.edu>
2016-09-25 22:56:33 -0500

    changed the timer so it could be reset.. tested on jsfiddle

--- master.js
+++ master.js
@@ -1,90 +1,118 @@
+var mns = document.getElementById('mns');
+var scs = document.getElementById('scs');
+var btcnt = document.getElementById('btnct');
+var reset = document.getElementById('reset');
+var showmns = document.getElementById('showmns');
+var showscs = document.getElementById('showscs');
+var t;
+
+var count = 0;
 var result;
 var userchoice;
 var userwins = 0;
 var botwins = 0;
 var ties = 0;
-var total = 0;
-var minutes = 0;
-var seconds = 0;
 var yourUl = document.getElementById("yourUlId");
-    
-    document.getElementById("submit").addEventListener("click", function(){
-    minutes = document.getElementById("minutes").value;
-    seconds = document.getElementById("seconds").value;
-    total = Number(minutes*60) + Number(seconds);
-    
-    var display = document.querySelector('#time'),
-        timer = new CountDownTimer(Number(total)),
-        timeObj = CountDownTimer.parse(Number(total));
-        
-        format(timeObj.minutes, timeObj.seconds);
-    
-    timer.onTick(format);
-    
-    timer.start();
-    
-    function format(minutes, seconds) {
-        if (minutes === 0 && seconds === 10){
-          document.getElementById("warning").style.display = '';
-        }
-        else if (minutes === 0 && seconds === 7){
-          document.getElementById("warning").style.display = 'none';
-        }
-        else if (minutes === 0 && seconds === 0){
-          if (userwins > botwins){
-            document.getElementById("winner").innerHTML = "user won.";
-          }
-          else if (userwins < botwins){
-            document.getElementById("winner").innerHTML = "bot won.";
-          }
-        }
 
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
-        display.textContent = minutes + ':' + seconds;
+document.getElementById("rock").addEventListener("click", function() {
+  play(this);
+});
+
+document.getElementById("paper").addEventListener("click", function() {
+  play(this);
+});
+
+document.getElementById("scissors").addEventListener("click", function() {
+  play(this);
+});
+
+function pad2(n) {
+  return n < 10 ? '0' + n : n;
+}
+
+function show() {
+  var s = count % 60;
+  var m = Math.floor(count / 60);
+  if (m === 0 && s === 10) {
+    document.getElementById("warning").style.display = '';
+  } else if (m === 0 && s === 7) {
+    document.getElementById("warning").style.display = 'none';
+  } else if (m === 0 && s === 0) {
+    if (userwins > botwins) {
+      document.getElementById("winner").innerHTML = "user won.";
+    } else if (userwins < botwins) {
+      document.getElementById("winner").innerHTML = "bot won.";
+    } else {
+      document.getElementById("winner").innerHTML = "it's a tie!";
     }
+  }
+  showmns.innerHTML = pad2(m);
+  showscs.innerHTML = pad2(s);
+}
+
+function timer() {
+  show();
+  if (count-- > 0) {
+    t = setTimeout(timer, 1000);
+  }
+}
+
+function setTimer() {
+  var s = parseInt(scs.value, 10);
+  var m = parseInt(mns.value, 10);
+  if (isNaN(s) || isNaN(m)) return;
+  scs.value = s;
+  mns.value = m;
+
+  var current = count;
+  count += (m * 60) + s;
+
+  // only restart the counter loop if it was previously stopped
+  if (current <= 0) {
+    timer();
+  } else {
+    show();
+  }
+}
+
+btcnt.addEventListener('click', function() {
+  setTimer();
 });
 
-document.getElementById("rock").addEventListener("click", function(){
-    play(this);
+reset.addEventListener('click', function() {
+  document.getElementById("userwins").innerHTML = userwins;
+  document.getElementById("botwins").innerHTML = botwins;
+  document.getElementById("ties").innerHTML = ties;
+  document.getElementById("winner").innerHTML = "";
+  userwins, botwins, ties, count = 0;
+  clearTimeout(t);
+  setTimer();
 });
 
-document.getElementById("paper").addEventListener("click", function(){
-    play(this);
-});
-
-document.getElementById("scissors").addEventListener("click", function(){
-    play(this);
-});
-
-function displayBotChoice(botchoice){
-  if (botchoice == 0){
+function displayBotChoice(botchoice) {
+  if (botchoice == 0) {
     document.getElementById("botchoice").innerHTML = "Rock";
-  }
-  else if (botchoice == 1){
+  } else if (botchoice == 1) {
     document.getElementById("botchoice").innerHTML = "Paper";
-  }
-  else{
+  } else {
     document.getElementById("botchoice").innerHTML = "Scissors";
   }
 }
 
-function play(userinput){
-    var botchoice = Math.floor(Math.random() * 3);
-    displayBotChoice(botchoice);
-    document.getElementById("userchoice").innerHTML = userinput.innerHTML;
-    
-    result = (3 + userinput.value - botchoice) % 3;
-    if (result == 1){
-        userwins++;
-        document.getElementById("userwins").innerHTML = userwins;
-    }
-    else if (result == 2){
-        botwins++;
-        document.getElementById("botwins").innerHTML = botwins;
-    }
-    else{
-        ties++;
-        document.getElementById("ties").innerHTML = ties;
-    }
+function play(userinput) {
+  var botchoice = Math.floor(Math.random() * 3);
+  displayBotChoice(botchoice);
+  document.getElementById("userchoice").innerHTML = userinput.innerHTML;
+
+  result = (3 + userinput.value - botchoice) % 3;
+  if (result == 1) {
+    userwins++;
+    document.getElementById("userwins").innerHTML = userwins;
+  } else if (result == 2) {
+    botwins++;
+    document.getElementById("botwins").innerHTML = botwins;
+  } else {
+    ties++;
+    document.getElementById("ties").innerHTML = ties;
+  }
 }
